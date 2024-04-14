1fb1eba449883d4b8cd99739a8372f74c8f295d8
mslisboa <mathewslysboa@gmail.com>
2024-04-14 20:02:51 -0300

    feat(estudo): 2024.1 - Parte 1 do projeto 2 referente a analisador léxico

---
+++ Conceitos de Linguagem de Programação/Proj2/Proj2a/analisador_lexico_decimal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool analisadorLexico(const string& palavra) {
+    char estado = false;
+
+    for(char caracter : palavra) {
+        switch(estado) {
+            case false:
+                if(caracter >= '0' && caracter <= '9') estado = true;
+
+            case true:
+                if(caracter < '0' || caracter > '9') {
+                    estado = false;
+                    return estado;
+                }
+        }
+    }
+
+    return estado;
+}
+
+
+int main() {
+    ifstream arquivo; // Abre o arquivo
+    string linha;
+    arquivo.open("palavras.txt");
+
+    if(arquivo.is_open()) { // Verifica se o arquivo foi aberto corretamente
+        while (getline(arquivo, linha)) { // Lê cada linha do arquivo
+            if(analisadorLexico(linha)) { // Chama a função analisadorLexico() para analisar a palavra
+                cout << "Palavra " << linha << ": reconhecida" << endl;
+            } else {
+                cout << "Palavra " << linha << ": rejeitada" << endl;
+            }
+        }
+
+        arquivo.close(); // Fecha o programa
+    } else {
+        cout << "Nao foi possivel abrir o arquivo." << endl;
+    }
+
+    return 0;
+}
